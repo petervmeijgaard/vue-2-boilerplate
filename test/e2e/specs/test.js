47877db7bb9730603d0a39b580a613c75039a484
Jonathan Soifer <me@jonathansoifer.com>
2017-08-13 18:36:39 +0300

    Implement basic End to End Testing

--- test/e2e/specs/test.js
+++ test/e2e/specs/test.js
@@ -11,9 +11,9 @@ module.exports = {
     browser
       .url(devServer)
       .waitForElementVisible('#app', 5000)
-      .assert.elementPresent('.hello')
-      .assert.containsText('h1', 'Welcome to Your Vue.js App')
-      .assert.elementCount('img', 1)
+      .assert.elementPresent('.panel')
+      .assert.containsText('h1', 'Login')
+      .assert.elementCount('a', 1)
       .end();
   },
 };
