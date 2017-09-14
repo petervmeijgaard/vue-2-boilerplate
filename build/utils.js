3c55d20eea196d310154f148e8dd6ccf739d61ba
Peter van Meijgaard <petervanmeijgaard@hotmail.com>
2017-09-14 14:39:24 +0200

    Fix typo

--- build/utils.js
+++ build/utils.js
@@ -23,7 +23,7 @@ exports.cssLoaders = function (options) {
   var postcssLoader = {
     loader: 'postcss-loader',
     options: {
-      sourceMap: options.sourcemap,
+      sourceMap: options.sourceMap,
     }
   }
 
