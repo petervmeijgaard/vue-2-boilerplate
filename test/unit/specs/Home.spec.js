9958fc8a68cc9a1688affcbe6af5eb5813a5eed0
Peter van Meijgaard <petervanmeijgaard@gmail.com>
2018-02-18 18:19:32 +1000

    Add unit testing

---
+++ test/unit/specs/Home.spec.js
@@ -0,0 +1,11 @@
+import { expect } from 'chai';
+import { mount } from '@vue/test-utils';
+import Home from '@/pages/Home/Index.vue';
+
+describe('Home index.vue', () => {
+  it('should render correct contents', () => {
+    const message = 'Welcome!';
+    const wrapper = mount(Home);
+    expect(wrapper.text()).to.include(message);
+  });
+});
